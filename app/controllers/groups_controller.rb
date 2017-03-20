2f90c8720cbdda13346bc21d964aa41235a518e9
linfgit <linfgg1@gmail.com>
2017-03-20 14:49:13 +0800

    add validates to title apply to edit/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,8 +10,11 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
-    redirect_to groups_path
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -24,8 +27,11 @@ class GroupsController < ApplicationController
 
   def update
     @group = Group.find(params[:id])
-    @group.update(group_params)
-    redirect_to groups_path, notice: "Update sucess"
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update sucess"
+    else
+      render :new
+    end
   end
 
   def destroy
