bcf6959aa552ff5c802f3f9a55620c7d756d2672
linfgit <linfgg1@gmail.com>
2017-03-20 14:40:40 +0800

    create groups crud

--- db/schema.rb
+++ db/schema.rb
@@ -21,9 +21,9 @@ ActiveRecord::Schema.define(version: 20170320061530) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
-    t.text     "desciption"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
